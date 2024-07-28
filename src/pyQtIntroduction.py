6faa7f3b4685a0b5c49624a24a4bf3dcdc1ebbaf
EvianMcKeown <evian.mck@gmail.com>
2024-07-28 15:44:36 +0200

    qtQML hello world

--- src/pyQtIntroduction.py
+++ src/pyQtIntroduction.py
@@ -1,6 +1,19 @@
-import sys
+import sys, os
 from pathlib import Path
+from PySide6 import QtGui
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6 import QtQuick
 
-print()
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    qml_file = Path(__file__).parent / "hello_world.qml"
+    engine.load(qml_file)
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
