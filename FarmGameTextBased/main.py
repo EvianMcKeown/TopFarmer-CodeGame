54b04514c4a0e6e98709d10c961a703ed6be9d75
Mustafa Mohamed <mhmmus009@myuct.ac.za>
2024-08-11 20:00:33 +0200

    Added inventory and more crops

--- FarmGameTextBased/main.py
+++ FarmGameTextBased/main.py
@@ -1,32 +1,34 @@
-# Text based farm game demo (main)
-# By Mustafa Mohamed
-# 2 August 2024
+# Text-based farm game demo
+# Author: Mustafa Mohamed
+# Created: 2 August 2024
+# Modified: 10 August 2024
 
 import sys
 from time import sleep
 from farm import *
 
-def main():
-    fg = FarmGrid(10, 10)
-    fg.add_farmer()
-    fg.print()
+FARM_WIDTH = 10
+FARM_HEIGHT = 10
 
-    while True:
-        instructions = input("Enter intructions: ").lower().split()
-        for ins in instructions:
-            match ins:
-                case "u" | "d" | "l" | "r":
-                    fg.farmer.move(ins[0])
-                case "pu" | "pd" | "pl" | "pr":
-                    fg.farmer.plant(ins[1])
-                case "hu" | "hd" | "hl" | "hr":
-                    fg.farmer.harvest(ins[1])
-                case "exit":
-                    sys.exit(0)
-                case _:
-                    print("Invalid instruction(s)")
-            sleep(0.5)
-            fg.print()
+# Initialise farm
+fg = FarmGrid(FARM_WIDTH, FARM_HEIGHT)
+fg.add_farmer(0, 0)
+fg.print()
 
-if __name__ == "__main__":
-    main()
+# Terminal input/output
+while True:
+    instructions = input("Enter intructions:").lower().split()
+    for ins in instructions:
+        match ins:
+            case "mu" | "md" | "ml" | "mr":
+                fg.farmer.move(ins[1])
+            case "pu" | "pd" | "pl" | "pr":
+                fg.farmer.plant(ins[1], 0)
+            case "hu" | "hd" | "hl" | "hr":
+                fg.farmer.harvest(ins[1])
+            case "exit":
+                sys.exit(0)
+            case _:
+                print("Invalid instruction(s)")
+        sleep(0.5)
+        fg.print()
--- FarmGameTextBased/mainGUI.py
+++ FarmGameTextBased/mainGUI.py
@@ -48,7 +48,13 @@ def render_farm():
                 case 2:
                     color = WATER
                 case 3:
-                    color = CROP
+                    match farm.grid[x][y].crop_type:
+                        case 0:
+                            color = POTATO
+                        case 1:
+                            color = CARROT
+                        case 2:
+                            color = PUMPKIN
                 case _:
                     color = BLACK
             if farm.farmer.get_pos() == (x, y):
@@ -60,18 +66,28 @@ def render_text_input(text):
     text = text.split('\n')
     for line_num in range(len(text)):
         text_image = pygame.font.SysFont(None, 24).render(text[line_num], True, GREEN)
-        pygame.draw.rect(surface, BLACK, pygame.Rect(0, line_num * 24, SIDE_WIDTH, 24 * line_num))
-        surface.blit(text_image, (0, (line_num) * 24))
+        pygame.draw.rect(surface, BLACK, pygame.Rect(0, SCALE_FACTOR + (line_num * 24), SIDE_WIDTH, 24 * line_num))
+        surface.blit(text_image, (0, SCALE_FACTOR + (line_num * 24)))
 
 def render_button_run():
-    text_image = pygame.font.SysFont(None, int(SCALE_FACTOR / 2)).render("RUN CODE", True, WHITE)
-    pygame.draw.rect(surface, BLUE, pygame.Rect(0, SCREEN_HEIGHT - SCALE_FACTOR, BUTTON_WIDTH, BUTTON_HEIGHT))
-    surface.blit(text_image, (SCREEN_WIDTH*0.005, (SCREEN_HEIGHT - SCALE_FACTOR)*1.04))
+    text_image = pygame.font.SysFont(None, int(SCALE_FACTOR / 2)).render("RUN", True, WHITE)
+    pygame.draw.rect(surface, GREEN, pygame.Rect(0, SCREEN_HEIGHT - SCALE_FACTOR, BUTTON_WIDTH, BUTTON_HEIGHT))
+    surface.blit(text_image, (7*SCREEN_WIDTH*0.005, (SCREEN_HEIGHT - SCALE_FACTOR)*1.04))
+
+def render_button_clear():
+    text_image = pygame.font.SysFont(None, int(SCALE_FACTOR / 2)).render("CLEAR", True, WHITE)
+    pygame.draw.rect(surface, BLUE, pygame.Rect(SCALE_FACTOR * 2, SCREEN_HEIGHT - SCALE_FACTOR, BUTTON_WIDTH, BUTTON_HEIGHT))
+    surface.blit(text_image, (27*SCREEN_WIDTH*0.005, (SCREEN_HEIGHT - SCALE_FACTOR)*1.04))
 
 def render_button_reset():
     text_image = pygame.font.SysFont(None, int(SCALE_FACTOR / 2)).render("RESTART", True, WHITE)
-    pygame.draw.rect(surface, RED, pygame.Rect(SCALE_FACTOR * 2, SCREEN_HEIGHT - SCALE_FACTOR, BUTTON_WIDTH, BUTTON_HEIGHT))
-    surface.blit(text_image, (25*SCREEN_WIDTH*0.005, (SCREEN_HEIGHT - SCALE_FACTOR)*1.04))
+    pygame.draw.rect(surface, RED, pygame.Rect(SCALE_FACTOR * 4, SCREEN_HEIGHT - SCALE_FACTOR, BUTTON_WIDTH, BUTTON_HEIGHT))
+    surface.blit(text_image, (47*SCREEN_WIDTH*0.005, (SCREEN_HEIGHT - SCALE_FACTOR)*1.04))
+
+def render_inventory():
+    text_image = pygame.font.SysFont(None, int(SCALE_FACTOR / 2)).render(str(farm.farmer.get_inventory()), True, BLACK)
+    pygame.draw.rect(surface, WHITE, pygame.Rect(0, 0, SIDE_WIDTH, BUTTON_HEIGHT))
+    surface.blit(text_image, (30, 15))
 
 def handle_button_run(instructions):
     instructions = instructions.lower().split('\n')
@@ -79,17 +95,26 @@ def handle_button_run(instructions):
         match ins:
             case "move up" | "move down" | "move left" | "move right":
                 farm.farmer.move(ins[5])
-            case "plant up" | "plant down" | "plant left" | "plant right":
-                farm.farmer.plant(ins[6])
+            case "plant potato up" | "plant potato down" | "plant potato left" | "plant potato right":
+                farm.farmer.plant(ins[13], 0)
+            case "plant carrot up" | "plant carrot down" | "plant carrot left" | "plant carrot right":
+                farm.farmer.plant(ins[13], 1)
+            case "plant pumpkin up" | "plant pumpkin down" | "plant pumpkin left" | "plant pumpkin right":
+                farm.farmer.plant(ins[14], 2)
             case "harvest up" | "harvest down" | "harvest left" | "harvest right":
                 farm.farmer.harvest(ins[8])
         render_all()
+        print(farm)
         time.sleep(1)
+
+def handle_button_clear():
+    global user_input
     user_input = ""
 
 def handle_button_reset():
     farm.__init__()
     farm.add_farmer()
+    global user_input
     user_input = ""
 
 def render_all():
@@ -97,7 +122,9 @@ def render_all():
     draw_grid(surface, SCREEN_WIDTH, SCREEN_HEIGHT, SCALE_FACTOR)
     render_text_input(user_input)
     render_button_run()
+    render_button_clear()
     render_button_reset()
+    render_inventory()
     pygame.display.flip()
 
 # main loop
@@ -123,6 +150,8 @@ while running:
             if 0 <= mouse[0] <= SCALE_FACTOR*2 and SCREEN_HEIGHT - SCALE_FACTOR <= mouse[1] <= SCREEN_HEIGHT:
                 handle_button_run(user_input)
             if SCALE_FACTOR*2 <= mouse[0] <= SCALE_FACTOR*4 and SCREEN_HEIGHT - SCALE_FACTOR <= mouse[1] <= SCREEN_HEIGHT:
+                handle_button_clear()
+            if SCALE_FACTOR*4 <= mouse[0] <= SCALE_FACTOR*6 and SCREEN_HEIGHT - SCALE_FACTOR <= mouse[1] <= SCREEN_HEIGHT:
                 handle_button_reset()
         
         render_all()
